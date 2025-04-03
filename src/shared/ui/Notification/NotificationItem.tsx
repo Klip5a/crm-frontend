e81dc21562805727dcf0572f950c394fc6622620
Klip5a <klip5a.dev@gmail.com>
2025-04-03 16:38:54 +0500

    add progress-bar animation to NotificationItem component and update styles

--- src/shared/ui/Notification/NotificationItem.tsx
+++ src/shared/ui/Notification/NotificationItem.tsx
@@ -23,8 +23,8 @@ const NotificationItem: React.FC<NotificationItemProps> = ({ notification }) => 
       initial={{ opacity: 0, y: -20 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: 20 }}
-      className={`pb-4 mb-2 rounded shadow
-            ${notification.type === "success" ? "bg-green " : ""}
+      className={`relative pb-4 mb-2 rounded shadow overflow-hidden
+            ${notification.type === "success" ? "bg-green-200 " : ""}
             ${notification.type === "error" ? "bg-red-200" : ""}
             ${notification.type === "warning" ? "bg-yellow-200" : ""}
             ${notification.type === "info" ? "bg-info-200" : ""}
@@ -32,6 +32,15 @@ const NotificationItem: React.FC<NotificationItemProps> = ({ notification }) => 
       onClick={() => notificationService.removeNotification(notification.id)}
     >
       {notification.message}
+      {notification.duration ? (
+        <div
+          className="animate-progress h-1.5 bg-green absolute bottom-0 left-0"
+          style={{
+            animationDuration: `${notification.duration}ms`,
+            width: "100%",
+          }}
+        />
+      ) : null}
     </motion.div>
   );
 };
