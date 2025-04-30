c41dd47463a7060bc334360a1a3fbf2acb4debff
Klip5a <klip5a.dev@gmail.com>
2025-04-30 17:00:01 +0500

    remove clientSlice

--- src/entities/client/model/clientSlice.ts
+++
@@ -1,9 +0,0 @@
-import { createEntityAdapter } from "@reduxjs/toolkit";
-
-import { Client } from "../types";
-
-const clientAdapter = createEntityAdapter<Client>({
-  selectId: (client: Omit<Client, "id">) => client.id,
-});
-
-const initialState = clientAdapter.getInitialState<{}>;
