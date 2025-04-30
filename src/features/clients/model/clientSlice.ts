9756c09725e37efee225f613e43652040d12f06e
Klip5a <klip5a.dev@gmail.com>
2025-04-30 17:03:38 +0500

    Added clientSlice.ts file with initial structure

---
+++ src/features/clients/model/clientSlice.ts
@@ -0,0 +1,11 @@
+// import { createEntityAdapter } from "@reduxjs/toolkit";
+
+// import { Client } from "../types";
+
+// export interface ClientState {}
+
+// const clientAdapter = createEntityAdapter<Client>({
+//   selectId: (client: Omit<Client, "id">) => client.id,
+// });
+
+// const initialState = clientAdapter.getInitialState<{}>;
