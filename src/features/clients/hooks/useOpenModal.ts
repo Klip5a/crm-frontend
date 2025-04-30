612b91e4a43f8189392c8be375d1d254849f3169
Klip5a <klip5a.dev@gmail.com>
2025-04-30 17:02:23 +0500

    Updated useOpenModal hook: replaced useAppDispatch with useCrmDispatch to improve modal window state management. Simplified the structure of returned functions for working with client modal windows.

--- src/features/clients/hooks/useOpenModal.ts
+++ src/features/clients/hooks/useOpenModal.ts
@@ -1,31 +1,24 @@
 import { Client } from "@entities/client";
-import { useAppDispatch } from "@shared/lib/redux";
 
-import {
-  closeModal,
-  openCreateModal,
-  openDeleteModal,
-  openEditModal,
-} from "../model/slice/modalSlice";
+import { useCrmDispatch } from "./crmRedux";
+
+import { closeModal, openCreateModal, openDeleteModal, openEditModal } from "../model/modalSlice";
 
 export function useOpenModal() {
-  const dispatch = useAppDispatch();
+  const dispatch = useCrmDispatch();
 
-  const createClient = () => {
-    dispatch(openCreateModal());
+  return {
+    createClientModal: () => {
+      dispatch(openCreateModal());
+    },
+    editClientModal: (client: Client) => {
+      dispatch(openEditModal(client));
+    },
+    deleteClientModal: (client: Client) => {
+      dispatch(openDeleteModal(client));
+    },
+    close: () => {
+      dispatch(closeModal());
+    },
   };
-
-  const editClient = (client: Client) => {
-    dispatch(openEditModal(client));
-  };
-
-  const deleteClient = (client: Client) => {
-    dispatch(openDeleteModal(client));
-  };
-
-  const close = () => {
-    dispatch(closeModal());
-  };
-
-  return { createClient, editClient, deleteClient, close };
 }
